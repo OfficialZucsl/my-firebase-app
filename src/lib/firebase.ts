b1cf682edeb18d68ea8840f25f071eea901932fd
chipeta texila <chipetatexila@gmail.com>
2025-08-29 14:33:30 +0000

    Nothing happens when i click login, the information just disappears. whe

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "fiducialend",
-  appId: "1:56482727919:web:ec41baf7bb9e66214b181a",
-  storageBucket: "fiducialend.firebasestorage.app",
-  apiKey: "AIzaSyCmZDIw34uiYqsmZsouOVbVqBlYHnibp-g",
-  authDomain: "fiducialend.firebaseapp.com",
-  messagingSenderId: "56482727919"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
