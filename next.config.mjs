738503ea4966d9ea67f8bb505ac4af90f662c627
chipeta texila <chipetatexila@gmail.com>
2025-09-09 11:11:35 +0000

    "My app is a Next.js web application written in TypeScript . I'm consist

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,9 @@
+import 'dotenv/config';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  experimental: {},
-  allowedDevOrigins: ['*.cloudworkstations.dev'],
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+  // Make the FIREBASE_SERVICE_ACCOUNT_KEY available to server-side code
   serverRuntimeConfig: {
-    // Will only be available on the server side
     FIREBASE_SERVICE_ACCOUNT_KEY: process.env.FIREBASE_SERVICE_ACCOUNT_KEY,
   },
 };
