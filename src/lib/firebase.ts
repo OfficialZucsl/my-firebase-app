b744ddb87a761ff7c492cff999559e46cae0f556
chipeta texila <chipetatexila@gmail.com>
2025-09-11 09:14:36 +0000

    App screen reads: Login Failed Firebase: Error (auth/api-key-not-valid.-

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,16 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCmZDIw34uiYqsmZsouOVbVqBlYHnibp-g",
+  authDomain: "fiducialend.firebaseapp.com",
+  projectId: "fiducialend",
+  storageBucket: "fiducialend.appspot.com",
+  messagingSenderId: "56482727919",
+  appId: "1:56482727919:web:ec41baf7bb9e66214b181a"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
